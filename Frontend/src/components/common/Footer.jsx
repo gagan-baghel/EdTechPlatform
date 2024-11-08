d9b7fb6f3ec9afeae7da9c9bcaa231fce84cb220
MACBOOK PRO <macbookpro@MACBOOKs-MacBook-Pro.local>
2024-11-08 21:04:38 +0530

    alldone

--- Frontend/src/components/common/Footer.jsx
+++ Frontend/src/components/common/Footer.jsx
@@ -171,7 +171,7 @@ const Footer = () => {
             })}
           </div>
 
-          <div className="text-center">Made with ❤️ CodeHelp © 2023 Studynotion</div>
+          <div className="text-center">Made with ❤️ GAGAN BAGHEL © 2023 Studynotion</div>
         </div>
       </div>
     </div>
--- Server/index.js
+++ Server/index.js
@@ -24,9 +24,9 @@ app.use(fileUpload({
     tempFileDir:"/tmp",
 }))
 app.use(cors({
-    origin:"http://localhost:3000",
-    credentials:true
-}))
+    origin: "*",
+    credentials: true
+}));
 
 connectDB()
 cloudinaryConnect()
